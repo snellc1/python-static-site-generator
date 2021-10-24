b67c0d07c8b5b3f733d8a3da7780eee56b5c5a43
Christopher Snell <chris.snell2019@gmail.com>
2021-10-24 14:04:45 -0700

    Create read() and write()

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -9,3 +9,10 @@ class Parser:
     
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
+
+    def read(self, path):
+        with open(path, "r") as file:
+            return file.read()
+    
+    def write(self, path, dest, content, ext=".html"):
+        full_path = self.dest / path.with_suffix(ext).name 
