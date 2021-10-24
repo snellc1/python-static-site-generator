8948d6b4cfeb5d7b32da2841b1bb252ced22178f
Christopher Snell <chris.snell2019@gmail.com>
2021-10-24 13:10:09 -0700

    Create class Site

--- ssg/site.py
+++ ssg/site.py
@@ -3,5 +3,5 @@ from pathlib import Path
 class Site:
 
     def __init__(self, source, dest):
-        self.source = Path()
-        self.dest = Path()
+        self.source = Path(source)
+        self.dest = Path(dest)
