d14bfd013f100ef90162ca4e1797ad4acbcfef0e
Christopher Snell <chris.snell2019@gmail.com>
2021-10-24 16:17:07 -0700

    Modify build()

--- ssg/site.py
+++ ssg/site.py
@@ -16,7 +16,9 @@ class Site:
 
         for path in self.source.rglob("*"):
             if path.is_dir():
-                self.create_dir(path)  
+                self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)  
     
     def load_parser(self, extension):
         for parser in self.parsers:
