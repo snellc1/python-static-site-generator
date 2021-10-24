071ea614884c9b0133d938f1aee5565ba0c27325
Christopher Snell <chris.snell2019@gmail.com>
2021-10-24 13:57:02 -0700

    Created valid_extension() and parse()

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -3,3 +3,9 @@ from pathlib import Path
 
 class Parser:
     extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+    
+    def parse(path: Path, source: Path, dest: Path):
+        raise NotImplementedError
