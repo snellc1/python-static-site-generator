cc98b97992ab465f9afd00f33aeba84377a18097
Christopher Snell <chris.snell2019@gmail.com>
2021-10-24 16:30:16 -0700

    Correct spelling of __delimiter

--- ssg/content.py
+++ ssg/content.py
@@ -3,5 +3,5 @@ from yaml import load, FullLoader
 from collections.abc import Mapping
 
 class Content(Mapping):
-    __delimeter = r"^(?:-|\+{3}\s*$"
-    __regex = re.compile(__delimeter, re.MULTILINE)
+    __delimiter = r"^(?:-|\+{3}\s*$"
+    __regex = re.compile(__delimiter, re.MULTILINE)
