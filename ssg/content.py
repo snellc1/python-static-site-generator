57abeab80f05f14fe1709b03e003cd3116cb6bfb
Christopher Snell <chris.snell2019@gmail.com>
2021-10-24 16:40:02 -0700

    Created load() and __init__()

--- ssg/content.py
+++ ssg/content.py
@@ -5,3 +5,13 @@ from collections.abc import Mapping
 class Content(Mapping):
     __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
+
+    @classmethod
+    def load(cls, string):
+        _, fm, content =  cls.__regex.split(string, 2)
+        metadata = load(fm, Loader=FullLoader)
+        return cls(metadata, content)
+
+    def __init__(self, metadata, content):
+        self.data = metadata
+        self.data["content"] = content
